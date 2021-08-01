9c38beb3a2be60a82d2dfe33e7f6e19e51957fe5
KH-20170630OXUD\Administrator <eamon3481@naver.com>
2021-08-01 23:01:40 +0900

    ee

--- app.js
+++ app.js
@@ -14,21 +14,42 @@ app.use(express.json());
 slackEvents.on('message', async (event) => {
  console.log(`메시지 수신 channel:${event.channel}, text:${event.text}`);
 });
-app.use('/slack/events', slackEvents.requestListener());
 
-app.post('/slack/events', (req, res) => {
- let event = req.body.event;
- if (req.body.challenge && req.body.type == 'url_verification') {
-  res.json({ challenge: req.body.challenge });
- }
+slackEvents.on('message', async (event) => {
+ console.log(
+  `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
+ );
 
-//  if (event.type === 'message') {
-//   console.log(`메시지 수신 channel:${event.channel}, user:${event.user}`);
-//  } 
+ // const result = await web.chat.postMessage({
+ //   // We'll add more functionality in the future. We just want to test it works, first
+ // 	text: 'This should output a leaderboard',
+ // 	channel: event.channel,
+ // });
+
+ // console.log(`Successfully send message ${result.ts} in conversation ${event.channel}`);
 });
+
+slackEvents.on('error', console.error);
+
+slackEvents.start(port).then(() => {
+ console.log(`server listening on port ${port}`);
+});
+
+// app.use('/slack/events', slackEvents.requestListener());
+
+// app.post('/slack/events', (req, res) => {
+//  let event = req.body.event;
+//  if (req.body.challenge && req.body.type == 'url_verification') {
+//   res.json({ challenge: req.body.challenge });
+//  }
+
+// //  if (event.type === 'message') {
+// //   console.log(`메시지 수신 channel:${event.channel}, user:${event.user}`);
+// //  }
+// });
 // 메지지 이벤트 엔드포인트를 express 에 등록하기
 
 // express 웹 서버 실행
-createServer(app).listen(3000, () => {
- console.log('run slack bot');
-});
+// createServer(app).listen(3000, () => {
+//  console.log('run slack bot');
+// });
