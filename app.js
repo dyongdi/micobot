442a7b9064ba56344cb210860119caad757d80ca
Dongmin Ahn <68339352+eamon3481@users.noreply.github.com>
2021-08-03 03:19:12 +0900

    micobot ver_0.1 (#1)

---
+++ app.js
@@ -0,0 +1,77 @@
+const { createEventAdapter } = require('@slack/events-api');
+const { createServer } = require('http');
+const { WebClient } = require('@slack/web-api');
+require('dotenv').config();
+
+// 슬랙에서 슬랙봇에게 접근가능한 엔드포인트를 만들기 위해 웹서버(express)를 사용
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+console.log(process.env.SLACK_SECRET, process.env.SLACK_BOT_TOKEN);
+if (!process.env.SLACK_SECRET) {
+ throw new Error('SLACK_SECRET is undefined');
+}
+
+if (!process.env.SLACK_BOT_TOKEN) {
+ throw new Error('SLACK_BOT_TOKEN is undefined');
+}
+
+const slackEvents = createEventAdapter(process.env.SLACK_SECRET);
+const web = new WebClient(process.env.SLACK_BOT_TOKEN);
+const til = require('./data.js');
+const channelName = 'C02AEDASRNC';
+const userName = new Set([
+ 'Q',
+ 'Daisy',
+ 'goody',
+ 'adela',
+ 'Seong',
+ 'eamon',
+ 'Tami',
+ 'autumn',
+ 'eve',
+ 'swing',
+]);
+const callName = {
+ Q: '@Q',
+ adela: '@Adela',
+ autumn: '@Autumn',
+ eve: '@eve',
+ goody: '@Goody',
+ swing: '@Swing',
+ eamon: '@Eamon',
+ Daisy: '@Daisy',
+ Tami: '@Tami',
+ Seong: '@Seong',
+};
+
+slackEvents.on('message', async (event) => {
+ console.log(event.channel);
+ if (event.channel === channelName && event.text === 'TIL') {
+  console.log(
+   `Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
+  );
+
+  const people = await til.git_til(userName, callName);
+  const result = await web.chat.postMessage({
+   text: `어제 안올린 사람: ${people}`,
+   channel: 'test',
+  });
+
+  console.log(
+   `Successfully send message ${result.ts} in conversation ${event.channel}`
+  );
+ }
+});
+
+slackEvents.on('error', console.error);
+
+app.use('/slack/events', slackEvents.requestListener());
+app.use(express.json());
+
+const server = createServer(app);
+server.listen(port, (req, res) => {
+ // Log a message when the server is ready
+ console.log(`Listening for events on ${server.address().port}`);
+ console.log(req);
+});
---
+++ data.js
@@ -0,0 +1,52 @@
+const axios = require('axios');
+
+const bodyParser = ({ body, base, user, number }) => {
+ const info = body.split('|');
+ if (info.length !== 16) return { id: -1 };
+ const link = info[14].match(/\([^)]+\)/)?.[0].replace(/[\(\)]/g, '');
+ return {
+  id: number,
+  link: link || '잘못된 링크입니다.',
+  condition: parseInt(info[13].trim()),
+  userImgUrl: user.avatar_url,
+  userName: base.ref,
+  date: info[11],
+ };
+};
+
+async function git_til(users, callName) {
+ process.env.TZ = 'Asia/Seoul';
+ const date = new Date();
+ let gapTime;
+
+ if (date.getDay() === 1) {
+  gapTime = date.getTime() - 24 * 60 * 60 * 1000 * 3;
+ } else {
+  gapTime = date.getTime() - 24 * 60 * 60 * 1000;
+ }
+ const offset = new Date().getTimezoneOffset() * 60000;
+
+ const preDate = new Date(gapTime - offset).toISOString().slice(0, 10);
+ console.log(preDate);
+ const result = await axios(
+  'https://api.github.com/repos/GleamingStar/miracle-coding/pulls?state=closed'
+ ).then((res) =>
+  res.data.map(bodyParser).filter(({ id, date }) => {
+   //    console.log(date, preDate, date === preDate);
+   return id !== -1 && date === preDate;
+  })
+ );
+ if (result) {
+  result.forEach(({ userName }) => {
+   if (users.has(userName)) {
+    users.delete(userName);
+   }
+  });
+ }
+
+ return Array.from(users)
+  .map((v) => callName[v])
+  .join(',');
+}
+
+module.exports.git_til = git_til;
